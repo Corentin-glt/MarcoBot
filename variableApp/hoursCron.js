ba59054bb44edfc454852cd761de05859977f4f2
simon <thome.simon@gmail.com>
2018-10-15 10:09:11 +0200

    package + cron

--- variableApp/hoursCron.js
+++ variableApp/hoursCron.js
@@ -3,7 +3,7 @@
  */
 
 const hoursCron = {
-  "morning": "48 * * * * *", //Une fois par jour à 08h30:00
+  "morning": "00 40 08 * * *", //Une fois par jour à 08h30:00
   "endAfterNoon": "00 30 * * * *",//Une fois par jour à 18h30:00
   "everyHour": "03 01 * * * *"// Toutes les heures
 };
