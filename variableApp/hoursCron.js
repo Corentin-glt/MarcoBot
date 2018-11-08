05c260a4535213ef666b19c14848860ef542a159
Simon Thome <s.thome@marcobot.io>
2018-11-08 18:11:34 +0100

    change send invitation hour

--- variableApp/hoursCron.js
+++ variableApp/hoursCron.js
@@ -6,7 +6,7 @@ const hoursCron = {
   "morning": "00 30 08 * * *", //Une fois par jour à 08h30:00
   "endAfterNoon": "00 30 15 * * *",//Une fois par jour à 18h30:00
   "everyHour": "03 01 * * * *",// Toutes les heures
-  "noon": "14 33 12 * * *" // 12:33 tous les jours
+  "noon": "14 13 12 * * *" // 12:33 tous les jours
 };
 
 module.exports = hoursCron;
