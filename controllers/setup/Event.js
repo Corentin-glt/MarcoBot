2f8797a8488805646893c3619089dc3352253e24
Simon Thome <s.thome@marcobot.io>
2018-11-12 13:17:42 +0100

    before merge

--- controllers/setup/Event.js
+++ controllers/setup/Event.js
@@ -1,6 +1,6 @@
 const ApiReferral = require('../../helpers/Api/apiReferral');
 const Messenger = require('./Messenger');
-const Nlp = require('../nlp/Nlp');
+const Nlp = require('../nlp2/Nlp');
 
 
 class Event {
